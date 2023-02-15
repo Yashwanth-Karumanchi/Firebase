baf59ae9bd74815bfd296b4d2a17d78a83fec263
Yashwant-Karumanhi <yashwanthkarumanchi@gmail.com>
2023-02-15 10:07:03 +0530

    Connection Established to Firebase using cdn

---
+++ scripts/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfZVPKarl7htW9olXZNNYbKa8vA7SsHj4",
+  authDomain: "sample-b0890.firebaseapp.com",
+  databaseURL: "https://sample-b0890-default-rtdb.firebaseio.com",
+  projectId: "sample-b0890",
+  storageBucket: "sample-b0890.appspot.com",
+  messagingSenderId: "712037800215",
+  appId: "1:712037800215:web:f574785d2ae47015cb7f52"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
